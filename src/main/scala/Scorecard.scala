e3d849bd7781784dc20303f57fc7456c7a1399dd
Andrew Shanks <andrew.shanks@digital.hmrc.gov.uk>
2017-04-07 18:57:23 +0100

    added simple roll validation

--- src/main/scala/Scorecard.scala
+++ src/main/scala/Scorecard.scala
@@ -2,13 +2,18 @@ package Scorecard
 /**
  * Created by andrew on 07/04/17.
  */
-class Scorecard(rollNumber: Int = 1) {
+class Scorecard(rollNumber: Int = 1, score: Int = 0) {
 
   def nextRoll:String = s"Frame 1: Roll $rollNumber"
-  def totalScore:Int = 0
+  def totalScore:Int = score
 
-  def roll(roll:Int): Scorecard = {
-    new Scorecard(2)
+  def roll(roll:Int): Either[String,Scorecard] = {
+    if (roll >= 0 && roll <= 10){
+      Right(new Scorecard(2, roll))
+    } else {
+      Left("Invalid roll")
+    }
+
   }
 
 }
--- src/test/scala/BowlingSpec.scala
+++ src/test/scala/BowlingSpec.scala
@@ -9,4 +9,44 @@ class ScorecardSpec   extends FlatSpec with Matchers {
     val myScorecard = new Scorecard
     myScorecard.nextRoll should be ("Frame 1: Roll 1")
   }
+
+  "A new scorecard" should "report the total score as 0" in {
+    val myScorecard = new Scorecard
+    myScorecard.totalScore should be (0)
+  }
+
+  "A new scorecard" should "respond to a roll of zero with a scorecard that reports the total score as 0" in  {
+    val myScorecard = new Scorecard
+    val nextScorecard = myScorecard.roll(0)
+    nextScorecard.isRight should be (true)
+    nextScorecard.right.get.totalScore should be (0)
+  }
+
+  "A new scorecard" should "respond to a roll of zero with a scorecard that reports the next roll as Frame 1: Roll 2" in  {
+    val myScorecard = new Scorecard
+    val nextScorecard = myScorecard.roll(0)
+    nextScorecard.isRight should be (true)
+    nextScorecard.right.get.nextRoll should be ("Frame 1: Roll 2")
+  }
+
+  "A new scorecard" should "respond to a roll of 1 with a scorecard that reports the total score as 1" in  {
+    val myScorecard = new Scorecard
+    val nextScorecard = myScorecard.roll(1)
+    nextScorecard.isRight should be (true)
+    nextScorecard.right.get.totalScore should be (1)
+  }
+
+  "A new scorecard" should "reject a roll of -1" in  {
+    val myScorecard = new Scorecard
+    val nextScorecard = myScorecard.roll(-1)
+    nextScorecard.isLeft should be (true)
+    nextScorecard.left.get should be ("Invalid roll")
+  }
+
+  "A new scorecard" should "reject a roll of 11" in  {
+    val myScorecard = new Scorecard
+    val nextScorecard = myScorecard.roll(11)
+    nextScorecard.isLeft should be (true)
+    nextScorecard.left.get should be ("Invalid roll")
+  }
 }
