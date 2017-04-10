dbd8d4f1107bc5af5ae418ce513649990bd24ad5
Andrew Shanks <andrew.shanks@digital.hmrc.gov.uk>
2017-04-10 12:50:52 +0100

    Implemented end of game without allowance for strikes/spares

--- src/main/scala/Scorecard.scala
+++ src/main/scala/Scorecard.scala
@@ -2,23 +2,35 @@ package Scorecard
 /**
  * Created by andrew on 07/04/17.
  */
-class Scorecard(frameNumber: Int = 1, rollNumber: Int = 1, score: Int = 0) {
 
-  def nextRoll:String = s"Frame $frameNumber: Roll $rollNumber"
+object GameParameters{
+  val PINS_IN_FRAME = 10
+  val FRAMES_IN_GAME = 10
+}
+
+class Scorecard(frameNumber: Int = 1, rollNumber: Int = 1, score: Int = 0, pinsLeft:Int = GameParameters.PINS_IN_FRAME) {
+
+  def nextRoll:String = {
+    if (frameNumber > GameParameters.FRAMES_IN_GAME) {
+      "Game Over"
+    } else {
+      s"Frame $frameNumber: Roll $rollNumber"
+    }
+  }
   def totalScore:Int = score
 
-  val PINS_IN_FRAME = 10
-
   def roll(roll:Int): Either[String,Scorecard] = {
-    if (roll >= 0 && roll <= (PINS_IN_FRAME-score)){
-      val frameRoll = if (rollNumber > 1) {
-        (frameNumber+1, 1)
+    if (frameNumber > GameParameters.FRAMES_IN_GAME){
+      Left(s"Game Over")
+    } else  if (roll >= 0 && roll <= pinsLeft){
+      val frameRollPins = if (rollNumber > 1 || roll == pinsLeft) {
+        (frameNumber+1, 1, GameParameters.PINS_IN_FRAME)
       } else {
-        (frameNumber, rollNumber + 1 )
+        (frameNumber, rollNumber + 1, GameParameters.PINS_IN_FRAME-roll)
       }
-      Right(new Scorecard(frameRoll._1, frameRoll._2, roll+score))
+      Right(new Scorecard(frameRollPins._1, frameRollPins._2, roll+score, frameRollPins._3))
     } else {
-      Left("Invalid roll")
+      Left(s"Invalid roll: should be between 0 and $pinsLeft")
     }
 
   }
