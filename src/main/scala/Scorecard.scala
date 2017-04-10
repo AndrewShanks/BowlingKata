8601b7b6905a4ed4663d14358001624162513d4a
Andrew Shanks <andrew.shanks@digital.hmrc.gov.uk>
2017-04-10 10:48:46 +0100

    Supporting frame advancement

--- src/main/scala/Scorecard.scala
+++ src/main/scala/Scorecard.scala
@@ -2,14 +2,21 @@ package Scorecard
 /**
  * Created by andrew on 07/04/17.
  */
-class Scorecard(rollNumber: Int = 1, score: Int = 0) {
+class Scorecard(frameNumber: Int = 1, rollNumber: Int = 1, score: Int = 0) {
 
-  def nextRoll:String = s"Frame 1: Roll $rollNumber"
+  def nextRoll:String = s"Frame $frameNumber: Roll $rollNumber"
   def totalScore:Int = score
 
+  val PINS_IN_FRAME = 10
+
   def roll(roll:Int): Either[String,Scorecard] = {
-    if (roll >= 0 && roll <= 10){
-      Right(new Scorecard(2, roll))
+    if (roll >= 0 && roll <= (PINS_IN_FRAME-score)){
+      val frameRoll = if (rollNumber > 1) {
+        (frameNumber+1, 1)
+      } else {
+        (frameNumber, rollNumber + 1 )
+      }
+      Right(new Scorecard(frameRoll._1, frameRoll._2, roll+score))
     } else {
       Left("Invalid roll")
     }
